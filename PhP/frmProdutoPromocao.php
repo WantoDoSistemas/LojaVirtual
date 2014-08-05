567a95abf26f8fea18f7f6a539fe91988add2c0f
diogobas <diogovvb@gmail.com>
2014-08-05 10:50:07 -0300

    ProdutoPromoca e ProdudosVisitados

---
+++ PhP/frmProdutoPromocao.php
@@ -0,0 +1,242 @@
+<html>
+
+<head>
+<title>Produto Promoção</title>
+<style>
+
+A {
+	text-decoration: underline;
+	color:FF7E00;
+	font-size: 10; 
+	font-family: arial;
+}
+TD{
+FONT-SIZE:11;
+FONT-FAMILY:arial;
+COLOR:000000;
+}
+
+#divLogo{
+	width: 100%;
+	height: 80px;
+	background-image: url("../images/fundo_topo.jpg");
+}
+
+#titulo{
+	font-size: 30px;
+	color: white;
+	margin-left: 15px;
+}
+
+#navMenu{
+	width: 100%;
+	height: 28px;	
+	margin-top:5px;
+	margin-left:5px;
+}
+
+#navMenu ul {
+	padding:0px;
+	margin:0px;	
+	list-style:none;
+}
+
+#navMenu ul li { 
+	display: inline; 
+
+}
+
+#navMenu ul li a {
+	padding: 2px 10px;
+	display: inline-block;
+ 
+	/* visual do link */
+	background-color:#EDEDED;
+	color: #333;
+	text-decoration: none;
+	font-size:14px;
+	border-bottom:3px solid #EDEDED;
+}
+
+#navMenu ul li a:hover {
+	background-color:#D6D6D6;
+	color: #6D6D6D;
+	border-bottom:3px solid #EA0000;
+}
+
+
+
+#navToolBar{
+	width: 100%;
+	height: 38px;	
+	margin-top:0px;
+	margin-left:0px;
+	
+	background-color:#EDEDED;
+}
+
+#navToolBar ul {
+	padding:0px;
+	margin-top:5px;
+	margin-left:25%;
+	list-style:none;	
+	width:48%;
+}
+
+#navToolBar ul li { 
+	display: inline; 
+}
+
+#navToolBar ul li a {
+	padding: 2px 5px;
+	display: inline-block;
+ 
+	/* visual do link */
+	
+	
+	text-decoration: none;
+
+	
+}
+
+#navToolBar ul li a:hover {	
+	border-bottom:3px solid #EA0000;
+}
+
+#tabForm{
+	margin-left:3%;
+	margin-top:35px;
+}
+
+#box-table-b {
+font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+font-size: 12px;
+text-align: center;
+border-collapse: collapse;
+border-top: 7px solid #9baff1;
+border-bottom: 7px solid #9baff1;
+margin: 20px;
+}
+
+#box-table-b th {
+font-size: 13px;
+font-weight: normal;
+background: #e8edff;
+border-right: 1px solid #9baff1;
+border-left: 1px solid #9baff1;
+border-bottom: 1px solid #aabcfe;
+color: #039;
+padding: 8px;
+}
+
+#box-table-b td {
+background: #e8edff;
+border-right: 1px solid #aabcfe;
+border-left: 1px solid #aabcfe;
+color: #669;
+padding: 8px;
+}
+
+#box-table-b tr:hover td {
+background: #d0dafd;
+color: #339;
+}
+
+</style>
+</head>
+
+<body LEFTMARGIN=0 TOPMARGIN=0 MARGINWIDTH="0" MARGINHEIGHT="0">
+<table height="100%" cellspacing="0" cellpadding="0" border="0">
+	<tr>
+		<td width="50%" background="../images/bg.jpg"></td>
+			  <td background="../images/bg_lft.jpg" width="28" valign="top">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+			  <td bgcolor="#FFFFFF" valign="top" height="100%">
+
+
+			<table border="0" cellpadding="0" cellspacing="0" width="800" height="100%">
+				<tr>
+					<td colspan="2">  <!--<img src="../images/slogan.jpg" border="0" width="800" height="110" alt=""> -->
+						<div id="divLogo" width="800" height="110">
+							<br>
+							<span id="titulo"><center>Produto Promoção</center></span>
+						</div>
+					</td>
+				</tr>
+				
+								
+				<tr>
+					<td colspan="2">
+						<form>
+							<table id=tabForm>																
+								<tr>									
+									<td>Categoria</td> 
+									<td>
+										<select id="cmbCategoria" name="cmbCategoria">
+											<option value=""></option>
+											<option value="Cama">Cama</option>
+											<option value="Mesa">Mesa</option>
+											<option value="Banho">Banho</option>
+										</select>
+									</td>
+								</tr>
+								<tr>									
+									<td>Produto</td> <td><input type="text" id="txtProduto" name="txtProduto" size="105px"> </td>
+									<td><img src="../images/find.ico"></td>
+								</tr>
+							</table>
+						</form>
+					</td>
+				</tr>
+				
+				<tr>
+					<td colspan="2">
+						<table id="box-table-b">
+							<tr>
+								<th scope="col" style="width:50px;">Código</th>								
+								<th scope="col" style="width:650px;">Produto</th>									
+								<th scope="col"></th>								
+							</tr>
+							<tr>
+								<td>0001</td>
+								<td style="text-align:left;">Mesa de Jantar</td>
+								<td><input type="checkbox" id="chkProduto_1" value="001"></td>									
+							</tr>
+							
+							<tr>
+								<td>0002</td>
+								<td style="text-align:left;">Armario Cozinha</td>
+								<td><input type="checkbox" id="chkProduto_2" value="002"></td>									
+							</tr>							
+						</table>
+					</td>
+				</tr>
+				
+				<tr>
+					<td colspan="2">
+						<table style="margin-left:40%;">
+							<tr>
+								<td><input type="submit" id="btnSalvar" name="btnSalvar" value="Salvar"></td> <td><input type="submit" id="btnCancelar" name="btnCancelar" value="Cancelar"></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				
+				<tr>
+					<td valign="top" height="100%">
+								
+					</td>
+					
+				</tr>
+				<tr>
+					<td background="../images/footer_2.jpg" height="44" align="center" valign="middle"><font style="font-size:9;font-family:verdana;font:900;color:666666">
+						Desenvolvido por <a href="http://wantodo.com.br">WanToDo</a> Sistemas. 2014
+					</td>
+				</tr>
+			</table>
+		</td>
+		
+		<td width="50%" background="../images/bg_lft.gif"></td>
+	</tr>
+</table>
+  </body>
+</html>
